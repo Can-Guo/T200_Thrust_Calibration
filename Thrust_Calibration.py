a3a3739007098ee5c7cb0c8309ab7b5be62134ba
Can-Guo <1739950387@qq.com>
2021-10-28 02:20:34 +0800

    20211028-1 initial commit

---
+++ Thrust_Calibration.py
@@ -0,0 +1,11 @@
+'''
+Date: 2021-10-27 09:13:34
+LastEditors: Guo Yuqin,12032421@mail.sustech.edu.cn
+LastEditTime: 2021-10-28 02:15:23
+FilePath: \thruster_thrust_calibration\Thrust_Calibration.py
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np 
+import matplotlib
+import xlrd 
